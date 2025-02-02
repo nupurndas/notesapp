b3821405a9ed5a89203846cfe0bd4a662d660dc4
nupur <nupur@32crayons.com>
2025-02-02 17:08:25 +0530

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Hey This the new app
       </div>
-      <h1>Vite + React</h1>
+      <h1>Change Amplily</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+         Let us check if this work
         </p>
       </div>
       <p className="read-the-docs">
